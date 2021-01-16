3aabe196cfe65b188b1ac1145c18abd718770e41
Phil McCarthy <philmcc2631@gmail.com>
2021-01-16 12:11:37 -0700

    Update create & show actions in users controller with sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
 
   def create
     new_user = User.create!(user_params)
-    flash[:success] = "Welcome, #{new_user}!"
+    flash[:success] = "Welcome, #{new_user.username}!"
+    session[:user_id] = new_user.id
     redirect_to root_path
+  end
+
+  def show
+    @user = current_user
   end
 
   private
