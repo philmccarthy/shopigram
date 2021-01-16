19c93d9e85d04e65c77b62c79c5763c4af5009c3
Phil McCarthy <philmcc2631@gmail.com>
2021-01-16 12:12:27 -0700

    Add users show and sessions new, create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
   resources :users, only: [:new, :create]
   
+  get '/profile', to: 'users#show', as: 'user_profile'
+  
+  get '/login', to: 'sessions#new', as: 'new_session'
+  post '/login', to: 'sessions#create', as: 'sessions'
 end
