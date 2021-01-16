e49c3921b06f5f94f158d1bb25825b265f9a0c08
Phil McCarthy <philmcc2631@gmail.com>
2021-01-16 10:06:01 -0700

    Add model test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # describe 'validations' do
-  #   it { should validate_presence_of :}
-  # end
-
-  # describe 'relationships' do
-  #   it {should belong_to :}
-  #   it {should have_many :}
-  # end
+  describe 'validations' do
+    it { should validate_presence_of :username }
+    it { should validate_uniqueness_of :username }
+    it { should validate_presence_of :password }
+  end
 
   describe 'roles' do
-    it 'can be created as an admin' do
-      user = User.create!(username: 'phil', password: 'password', role: 1)
+    xit 'can be created as an admin' do
+      user = User.create!(username: 'phil',
+                          password: 'password',
+                          role: 1)
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+
+    xit 'can be created as a default user' do
+      user = User.create!(username: 'alex',
+                          password: 'password',
+                          role: 0)
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
   end
 end
