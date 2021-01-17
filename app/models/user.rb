dacc87e1e68a038faa9bb45764d85f607e8c0079
Phil McCarthy <philmcc2631@gmail.com>
2021-01-17 12:30:42 -0700

    Add User class method ::community (AR query)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many_attached :images
+
+  def self.community
+    joins(:images_attachments)
+      .group(:id)
+      .having("COUNT(active_storage_attachments) >= 3")
+      .order(Arel.sql("RANDOM()"))
+      .first(5)
+  end
 end
