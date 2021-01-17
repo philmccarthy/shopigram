43aa7704deb7d1342357cba9b498c64bc15aede4
Phil McCarthy <philmcc2631@gmail.com>
2021-01-16 17:42:19 -0700

    Update routes for users and login

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :update, :destroy]
   
   get '/profile', to: 'users#show', as: 'user_profile'
   
   get '/login', to: 'sessions#new', as: 'new_session'
-  post '/login', to: 'sessions#create', as: 'sessions'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
