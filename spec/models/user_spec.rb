2247e5e9fc48c5c52ef929af049ab5fe03b815f9
Phil McCarthy <philmcc2631@gmail.com>
2021-01-17 12:29:03 -0700

    Add model test for User.community class method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,21 +7,36 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
   end
 
-  describe 'roles' do
-    xit 'can be created as an admin' do
-      user = User.create!(username: 'phil',
-                          password: 'password',
-                          role: 1)
-      expect(user.role).to eq("admin")
-      expect(user.admin?).to be_truthy
+  describe 'activestorage attachments' do
+    it 'it can have images attached' do
+      user = User.create!(username: 'Phil', password: 'password')
+      user.images.attach(
+        io: File.open(Rails.root + 'spec/files/test/junior-1615.jpg'),
+        filename: 'img.jpg',
+        content_type: 'image/*'
+      )
+      expect(user.images).to be_attached
     end
+  end
 
-    xit 'can be created as a default user' do
-      user = User.create!(username: 'alex',
-                          password: 'password',
-                          role: 0)
-      expect(user.role).to eq("default")
-      expect(user.default?).to be_truthy
+  describe 'class methods' do
+    it '::community returns 5 random users with >= 3 images' do
+      User.create!(username: 'Phil', password: 'password')
+      User.create!(username: 'Alex', password: 'password')
+      User.create!(username: 'Patrick', password: 'password')
+      User.create!(username: 'Mick', password: 'password')
+      User.create!(username: 'Tom', password: 'password')
+      User.create!(username: 'Sheila', password: 'password')
+      User.all.each do |user|
+        3.times do
+          user.images.attach(
+            io: File.open(Rails.root + 'spec/files/test/junior-1615.jpg'),
+            filename: 'img.jpg',
+            content_type: 'image/*')
+          end
+        end
+      expect(User.community.size).to eq(5)
+      expect(User.third.images.size).to eq(3)
     end
   end
 end
