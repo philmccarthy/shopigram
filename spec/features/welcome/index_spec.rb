fd65ae9783b0d59b1b7b6d9e316eceec500d5ee0
Phil McCarthy <philmcc2631@gmail.com>
2021-01-16 17:42:01 -0700

    Update welcome#index feature test for file upload, WIP

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -37,5 +37,32 @@ RSpec.describe 'welcome index', type: :feature do
       expect(current_path).to eq(new_session_path)
       expect(page).to have_content("Username or password doesn't match our records.")
     end
+
+    it 'can logout' do
+      click_on 'Log In'
+      
+      within('#login-form') do
+        fill_in 'session[username]', with: user.username
+        fill_in 'session[password]', with: user.password
+        click_on 'Log In'
+      end
+
+      visit root_path
+
+      click_on 'Logout'
+
+      expect(page).to_not have_content('Profile')
+      expect(page).to have_content('Log In')
+    end
+
+    it 'can upload images' do
+      visit root_path
+
+      click_on 'Browse Files...'
+
+      attach_file('images[]', '/app/images/test/junior-1615.jpg')
+
+      expect(page).to have_content('test_image.jpg')
+    end
   end
 end
