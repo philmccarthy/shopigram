8adbb7c22d0066db0231b44d35599f16635455a1
Phil McCarthy <philmcc2631@gmail.com>
2021-01-17 14:12:39 -0700

    Add order_by_date inst. method for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-
   has_secure_password
-
   has_many_attached :images
 
   def self.community
@@ -12,5 +10,9 @@ class User < ApplicationRecord
       .having("COUNT(active_storage_attachments) >= 3")
       .order(Arel.sql("RANDOM()"))
       .first(5)
+  end
+
+  def images_by_date
+    images.order(created_at: :desc)
   end
 end
